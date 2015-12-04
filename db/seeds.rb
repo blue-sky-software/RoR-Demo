967ffbebabe93125e1d7d8c1d35df6a0dc984654
root <blue-sky-software>
2015-12-05 06:51:25 +0800

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(name: 'alex', fullname: 'Alex Nilson', point: 1234, notes: 'power user')
+User.create(name: 'gist124', fullname: 'Nick Roberts', point: 62, notes: 'new user')
+User.create(name: 'goldweb', fullname: 'Sergio Jackson', point: 132, notes: 'edit user')
+User.create(name: 'lisa', fullname: 'Lisa Wong', point: 434, notes: 'new user')
+User.create(name: 'nick12', fullname: 'Nick Roberts', point: 232, notes: 'power user')
+User.create(name: 'webriver', fullname: 'Antoino Sanches', point: 462, notes: 'new user')
